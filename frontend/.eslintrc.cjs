1b9cc60751c28a0f67a53dd47504210cee134fbf
mqnu00 <647983952@qq.com>
2026-05-27 20:36:16 +0800

    chore: add eslint, prettier and stylelint configuration files
    - Root: .prettierrc, .prettierignore
    - Backend: .eslintrc.json, .eslintignore
    - Frontend: .eslintrc.cjs, .eslintignore, .stylelintrc.json

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,47 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:vue/vue3-essential',
+    'prettier',
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    // Vue 相关规则
+    'vue/multi-word-component-names': 'off',
+    'vue/no-v-html': 'warn',
+    'vue/require-default-prop': 'off',
+    'vue/require-explicit-emits': 'warn',
+
+    // TypeScript 相关规则
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+
+    // 通用规则
+    'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
+    'no-debugger': 'warn',
+    'prefer-const': 'error',
+    'no-var': 'error',
+  },
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly',
+  },
+};
